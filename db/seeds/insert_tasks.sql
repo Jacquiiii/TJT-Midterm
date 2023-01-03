50294223eac605f788e336f289dfe7385241b31b
Tammy-Lee <tammy-lee.plamondon@telus.com>
2023-01-03 09:13:48 -0800

    refactored code and names of files

---
+++ db/seeds/insert_tasks.sql
@@ -0,0 +1,14 @@
+INSERT INTO tasks (name, category)
+VALUES
+  ("Milk", "buy"),
+  ("Bread", "buy"),
+  ("Eggs", "buy"),
+  ("The 100", "read"),
+  ("To Kill a Mockingbird", "read"),
+  ("Rose Red", "read"),
+  ("Pizza", "eat"),
+  ("Chinese food", "eat"),
+  ("Sushi", "eat"),
+  ("The Shawshank Redemption", "watch"),
+  ("The Godfather", "watch"),
+  ("The Dark Knight", "watch");
