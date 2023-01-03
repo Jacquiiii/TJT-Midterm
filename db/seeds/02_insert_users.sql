e43870bb826e5744d61e7565fb8d7e99b68ae63c
Tammy-Lee <tammy-lee.plamondon@telus.com>
2023-01-03 15:46:46 -0800

    fixed brokeness

---
+++ db/seeds/02_insert_users.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO users (username, email, password) VALUES ('Alice Cooper', 'alice@wonderland.ca', 'password', 'MadHatter');
+INSERT INTO users (username, email, password) VALUES ('Kira Knight Lee', 'kira@caribbean.ca', 'password', 'JarOfDirt');
+
+
+INSERT INTO todos (task, category, completed) VALUES ('Buy milk', 'Buy', false);
+
--- db/seeds/insert_users.sql
+++
@@ -1,7 +0,0 @@
-
-INSERT INTO users (name) VALUES ('Alice Cooper', 'alice@wonderland.ca', 'password', 'MadHatter');
-INSERT INTO users (name) VALUES ('Kira Knight Lee', 'kira@caribbean.ca', 'passwords', 'JarOfDirt');
-
-
-INSERT INTO todos (task, category, completed) VALUES ('Buy milk', 'Buy', false);
-
