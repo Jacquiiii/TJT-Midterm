c5ef10ca01d52ae1ba52b91decf0e13118c9f6cf
Tammy-Lee <tammy-lee.plamondon@telus.com>
2023-01-03 08:50:08 -0800

    add update to schema and seeds

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -5,3 +5,13 @@ CREATE TABLE todos (
   category varchar(255) NOT NULL,
   completed boolean DEFAULT false
 );
+
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL
+);
