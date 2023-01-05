da89adb00b81311db0482a6cc55faca157d93c6a
Tony Eng <tony.eng@telus.com>
2023-01-05 15:13:13 -0800

    add change category functionality

--- server.js
+++ server.js
@@ -85,6 +85,19 @@ app.post('/delete', function(req, res) {
   .catch((err) => res.send(err));
 })
 
+app.post('/change', (req, res) => {
+  console.log("testing req.body", req.body);
+  const value = [req.body.changecategory, req.body.taskid];
+  const changeQuery = `
+  UPDATE tasks SET category = $1
+  WHERE id = $2;
+  `
+  db.query(changeQuery, value)
+    .then((result) => {
+      res.redirect("/");
+    })
+})
+
 
 app.post('/login', (req, res) => {
   userEmailQueries.getUserByEmail(req.body.email)
